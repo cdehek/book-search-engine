32e07ff5f8ff1bfa8e4fb3c7d0180ec9910e575f
Christian <christian.dehek@gmail.com>
2021-01-09 14:16:21 -0700

    added queries and mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -43,7 +43,7 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(book: BookInput): User
+        saveBook(book: BookInput!): User
         removeBook(bookId: String!): User
     }
 `;
